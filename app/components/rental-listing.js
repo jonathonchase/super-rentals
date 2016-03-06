e4951046da8fdbf16b74a8b7371f2651375cf084
jonathonchase <jonathonchase@gmail.com>
2016-03-06 14:33:33 -0800

    Wired up the image set by adding a handle-barred action reference within the button's opening tag. Added js implementation within the component under the actions section.

--- app/components/rental-listing.js
+++ app/components/rental-listing.js
@@ -1,5 +1,11 @@
 import Ember from 'ember';
 
 export default Ember.Component.extend({
-  isImageShowing: false
+  isImageShowing: false,
+  actions: {
+    imageShow() {
+      this.set('isImageShowing', true);
+    }
+  }
+
 });
