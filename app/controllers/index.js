888696fa88718c06bc7f314858ec93db595ca059
jonathonchase <jonathonchase@gmail.com>
2016-03-06 17:05:57 -0800

    Fixed a typo in my TODO.
    TODO: Change up the webstorm color scheme to be more friendly to my awesome sight powers.

--- app/controllers/index.js
+++ app/controllers/index.js
@@ -1,4 +1,27 @@
 import Ember from 'ember';
 
+//TODO: Determine what the idiomatic method of turning the below action implementations into a single DRY function
+//Currently, these implementations are identical other than their target, which is pretty eh.
 export default Ember.Controller.extend({
+  filteredList: null,
+  actions: {
+    autoComplete(param) {
+      if(param !== "") {
+        this.store.query('rental', {city: param}).then((result) => {
+          this.set('filteredList',result);
+        });
+      } else {
+        this.set('filteredList').clear();
+      }
+    },
+    search(param) {
+      if(param !== "") {
+        this.store.query('rental', {city: param}).then((result) => {
+          this.model.set('model',result);
+        });
+      } else {
+        this.set('model').clear();
+      }
+    }
+  }
 });
